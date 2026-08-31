66d1a32b98f4cb975f4ebc4ea0af8445866026ac
rahanyas <rahanyas3@gmail.com>
2026-08-31 20:16:50 +0530

    creating context for msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import SignUpPage from "./pages/SignIn";
-import Message from "./components/shared/Message";
 
 function LandingRoute() {
   const navigate = useNavigate();
@@ -34,11 +33,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingRoute />} />
         <Route path="/app" element={<DashboardRoute />} />
-        <Route path='/sign-in' element={
-          <SiginInRoute>
-            <Message />
-          </SiginInRoute>
-          }/>
+        <Route path='/sign-in' element={<SiginInRoute/> }/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/context/Message.context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react";
+
+
+const MessageContext = createContext();
+
+export const MessageProvider = ({children}) => {
+
+  const [message, setMessage] = useState({
+    text : '',
+    type : ''
+  });
+
+  const showMessage = (text, type) => {
+    setMessage({
+      text, 
+      type
+    })
+  };
+
+  const clearMessage = () => {
+    setMessage({
+      text : '',
+      type: ''
+    })
+  };
+
+  return(
+    <MessageContext.Provider 
+     value={{
+        message,
+        showMessage,
+        clearMessage
+      }}
+    >
+     {children}
+    </MessageContext.Provider>
+  )
+};
+
+
+export const useMessage = () => {
+  return useContext(MessageContext)
+};
+
+
